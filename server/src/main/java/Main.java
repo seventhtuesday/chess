4d876765a392fcd5104b5193b90f7cda27ceaf6a
Trent Lawrence Ence <trence13@et.byu.edu>
2024-10-21 12:08:38 -0600

    phase 3 started

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,8 @@
-import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        Server Sevy = new Server();
+        Sevy.run(8080);
     }
 }
