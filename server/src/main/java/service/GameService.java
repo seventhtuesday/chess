354dd13c3e2c29fa3bf9d00dd72e8ffc154c8be3
seventhtuesday <trence13@byu.edu>
2024-10-22 19:04:48 -0700

    game create complete

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,4 +1,46 @@
 package service;
 
+import chess.ChessGame;
+import dataAccess.AuthDAO;
+import dataAccess.DataAccessException;
+import dataAccess.GameDAO;
+import model.AuthData;
+import model.CreateRequest;
+import model.CreateResult;
+import model.GameData;
+
+import java.util.Random;
+
 public class GameService {
+    private AuthDAO authDAO;
+    private GameDAO gameDAO;
+
+    public GameService(GameDAO gameDAO, AuthDAO authDAO) {
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+    }
+
+    public CreateResult createGame(CreateRequest create) throws Exception {
+        //verify not bad request
+        if(create.authToken() == null || create.gameName() == null){
+            throw new Exception("error: bad request");
+        }
+        //verify authToken
+        if(authDAO.getAuth(create.authToken()) == null) {
+            throw new Exception("error: unauthorized");
+        }
+        try {
+            //generate random game ID
+            Random generator = new Random();
+            int gameID = generator.nextInt();
+            //create the game
+            ChessGame game = new ChessGame();
+            GameData data = new GameData(gameID, null, null, create.gameName(), game);
+            //add game to database
+            gameDAO.createGame(data);
+            return new CreateResult(gameID);
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
 }
---
+++ server/src/test/java/service/GameTest.java
@@ -0,0 +1,40 @@
+package service;
+
+import dataAccess.AuthDAO;
+import dataAccess.GameDAO;
+import model.AuthData;
+import model.CreateRequest;
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+    static GameDAO gameDAO = new GameDAO();
+    static AuthDAO authDAO = new AuthDAO();
+    static GameService gameS = new GameService(gameDAO, authDAO);
+
+
+
+    @BeforeEach
+    void clear() throws Exception {
+        gameDAO.clear();
+    }
+
+    @Test
+    void createGameGood() {
+        //create user
+        UserData user = new UserData("good", "pass", "email@mail.com");
+        //create auth for user
+        AuthData data = authDAO.createAuth(user);
+        //test create game
+        CreateRequest create = new CreateRequest("test", data.authToken());
+        Assertions.assertDoesNotThrow(() -> gameS.createGame(create));
+    }
+
+    @Test
+    void createGameBad() {
+        CreateRequest create = new CreateRequest(null,null);
+        Assertions.assertThrows(Exception.class, () -> gameS.createGame(create));
+    }
+}
