fa7d578a1d0c08f0df107862e73de63ab432900d
seventhtuesday <trence13@byu.edu>
2024-10-22 20:19:19 -0700

    list game done

--- server/src/main/java/service/ListService.java
+++ server/src/main/java/service/ListService.java
@@ -1,4 +1,37 @@
 package service;
 
+import dataAccess.AuthDAO;
+import dataAccess.DataAccessException;
+import dataAccess.GameDAO;
+import model.AuthData;
+import model.GameData;
+import model.GameResult;
+
+import java.util.ArrayList;
+
 public class ListService {
+    private AuthDAO authDAO;
+    private GameDAO gameDAO;
+
+    public ListService(AuthDAO authDAO, GameDAO gameDAO) {
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+    }
+
+    public ArrayList<GameResult> list(AuthData request) throws Exception {
+        //verify authToken
+        if(authDAO.getAuth(request.authToken()) == null) {
+            throw new Exception("error: unauthorized");
+        }
+        try {
+            ArrayList<GameData> temp = gameDAO.getAllGames();
+            ArrayList<GameResult> result = new ArrayList<>();
+            for (GameData game : temp) {
+                result.add(new GameResult(game.gameID(), game.whiteUsername(), game.blackUsername(), game.gameName()));
+            }
+            return result;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
 }
--- server/src/test/java/service/ListTest.java
+++ server/src/test/java/service/ListTest.java
@@ -1,4 +1,62 @@
 package service;
 
+import chess.ChessGame;
+import dataAccess.AuthDAO;
+import dataAccess.GameDAO;
+import model.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 public class ListTest {
+    static GameDAO gameDAO = new GameDAO();
+    static AuthDAO authDAO = new AuthDAO();
+    static ListService listS = new ListService(authDAO, gameDAO);
+
+    @BeforeEach
+    void clear() {
+        gameDAO.clear();
+        authDAO.clear();
+    }
+
+    @Test
+    void listGameGood() {
+        //create user
+        UserData user = new UserData("good", "pass", "email@mail.com");
+        //create auth for user
+        AuthData auth = authDAO.createAuth(user);
+        //create games
+        GameData game = new GameData(1234, null, null, "test", new ChessGame());
+        GameData game2 = new GameData(5678, null, null, "test2", new ChessGame());
+        gameDAO.createGame(game);
+        gameDAO.createGame(game2);
+        ArrayList<GameResult> start = new ArrayList<>();
+        start.add(new GameResult(game.gameID(), game.whiteUsername(), game.blackUsername(), game.gameName()));
+        start.add(new GameResult(game2.gameID(), game.whiteUsername(), game2.blackUsername(), game2.gameName()));
+        //test listing
+        try {
+            ArrayList<GameResult> result = listS.list(auth);
+            if(!result.equals(start)) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    void listGameEmpty() {
+        //create user
+        UserData user = new UserData("good", "pass", "email@mail.com");
+        //create auth for user
+        AuthData auth = authDAO.createAuth(user);
+        //test that will return empty set
+        try {
+            Assertions.assertTrue(listS.list(auth).isEmpty());
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+    }
 }
