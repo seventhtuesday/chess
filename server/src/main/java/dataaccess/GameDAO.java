95aa82ed46185ea6d37d251b5e4e3f98af80785c
seventhtuesday <trence13@byu.edu>
2024-11-01 13:33:35 -0600

    game create, get, & clear

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,7 +1,14 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
+import model.GameData;
+
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class GameDAO {
     private Connection conn;
 
@@ -13,9 +20,9 @@ public class GameDAO {
                 String stmt = """
                         CREATE TABLE IF NOT EXISTS GAME (
                             `ID` int NOT NULL,
-                            `WHITENAME` varchar(255),
-                            `BLACKNAME` varchar(255),
-                            `GAMENAME` varchar(255) NOT NULL,
+                            `WHITE` varchar(255),
+                            `BLACK` varchar(255),
+                            `NAME` varchar(255) NOT NULL,
                             `JSON` TEXT NOT NULL
                         """;
                 try (var s = conn.prepareStatement(stmt)) {
@@ -26,6 +33,64 @@ public class GameDAO {
             }
         } catch (DataAccessException e) {
             throw new Exception(String.format("Database Failure: %s", e.getMessage()));
+        }
+    }
+
+    public void createGame(GameData game) throws DataAccessException {
+        try {
+            var s = conn.prepareStatement("INSERT INTO GAME (ID, WHITE, BLACK, NAME, JSON)");
+
+            s.setString(1, String.valueOf(game.gameID()));
+            s.setString(2, game.whiteUsername());
+            s.setString(3, game.blackUsername());
+            s.setString(4, game.gameName());
+
+            var json = new Gson().toJson(game.game());
+            s.setString(5, json);
+
+            s.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Database Failure: %s", e.getMessage()));
+        }
+    }
+
+    public GameData getGame(int gameID) throws DataAccessException {
+        try {
+            var s = conn.prepareStatement("SELECT * FROM GAME WHERE ID = " + gameID);
+            s.setString(1, String.valueOf(gameID));
+            ResultSet rs = s.executeQuery();
+            if (rs.next()) {
+                String white = rs.getString("WHITE");
+                String black = rs.getString("BLACK");
+                String name = rs.getString("NAME");
+
+                String json = rs.getString("JSON");
+                ChessGame game = new Gson().fromJson(json, ChessGame.class);
+
+                return new GameData(gameID, white, black, name, game);
+            }
+            else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Database Failure: %s", e.getMessage()));
+        }
+    }
+
+    public ArrayList<GameData> getAllGames() throws DataAccessException {
+
+    }
+
+    public void updateGame(GameData game) throws DataAccessException {
+
+    }
+
+    public void clear() throws DataAccessException {
+        try {
+            var s = conn.prepareStatement("TRUNCATE TABLE GAME");
+            s.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Database Failure: %s", e.getMessage()));
         }
     }
 
