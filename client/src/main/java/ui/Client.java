9e5bb339ce10044d49a64f11314053b36d5a7847
seventhtuesday <trence13@byu.edu>
2024-11-14 13:05:54 -0700

    facade

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -1,8 +1,103 @@
 package ui;
 
+import chess.ChessGame;
+import model.*;
 import server.ServerFacade;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static ui.EscapeSequences.*;
+
 public class Client {
-    public UserState uState;
+    public static UserState uState;
     private ServerFacade sv;
+    private String url;
+    private LoopR loop;
+    private ArrayList<GameResult> games;
+    private ConcurrentHashMap<Integer, GameData> gameObj;
+    private GameData game;
+    private ChessGame.TeamColor team;
+    private AuthData auth;
+
+    public Client(String url, LoopR loop) {
+        uState = UserState.LOGGED_OUT;
+        this.url = url;
+        this.loop = loop;
+        sv = new ServerFacade(url);
+    }
+
+    public String out(String in) {
+        try {
+            var args = in.split(" ");
+            var command = (args.length > 0) ? args[0].toLowerCase() : "help";
+            var params = Arrays.copyOfRange(args, 1, args.length);
+
+            return switch (command) {
+                case "register" -> register(params);
+                case "login" -> login(params);
+                case "logout" -> logout(params);
+                case "create" -> create(params);
+                case "join" -> join(params);
+                case "list" -> list(params);
+                case "observe" -> observe (params);
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    private String register(String[] params) {
+        if (uState != UserState.LOGGED_OUT) {
+            return "Logout before registering a new user";
+        }
+
+        if (params.length == 3) {
+            UserData user = new UserData(params[0], params[1], params[2]);
+            try {
+                auth = sv.register(user);
+            } catch (Exception e) {
+                return "bad request: one of your parameters could not be accepted";
+            }
+            uState = UserState.LOGGED_IN;
+            return "Logged in as " + auth.username();
+        }
+
+        return "bad request: double check parameters";
+    }
+
+    private String help() {
+        String[] cmds = {};
+        String[] descrip = {};
+
+        if(uState == UserState.LOGGED_OUT) {
+            cmds = new String[] {"register <USERNAME> <PASSWORD> <EMAIL>", "login <USERNAME> <PASSWORD>", "quit", "help"};
+            descrip = new String[] {"create an account", "login to existing account", "quit the app", "list available commands"};
+        }
+        else {
+            cmds = new String[] {"create <NAME>", "list", "join <ID> [WHITE|BLACK|<empty>]", "observe <ID>", "logout", "quit", "help"};
+            descrip = new String[]{"create a game with provided name",
+                    "list all games",
+                    "joins the provided game to play as the provided team",
+                    "watch the provided game",
+                    "logs out the current user",
+                    "quit the app",
+                    "list available commands"};
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for (int n = 0; n < cmds.length; n++) {
+            ans.append(SET_TEXT_COLOR_BLUE);
+            ans.append(" - ");
+            ans.append(cmds[n]);
+            ans.append(SET_TEXT_COLOR_MAGENTA);
+            ans.append(" - ");
+            ans.append(descrip[n]);
+            ans.append("\n");
+        }
+        return ans.toString();
+    }
 }
--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,4 +1,112 @@
 package server;
 
+import com.google.gson.Gson;
+import model.*;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class ServerFacade {
+    private final String url;
+    private String token;
+
+    public ServerFacade(String url) {
+        this.url = url;
+    }
+
+    public AuthData register(UserData user) throws Exception {
+        String path = "/user";
+        AuthData authData = this.request("POST", path, user, AuthData.class);
+        if (authData != null) {
+            token = authData.authToken();
+        }
+        return authData;
+    }
+
+    public AuthData login(UserData user) throws Exception {
+        String path = "/session";
+        AuthData authData = this.request("POST", path, user, AuthData.class);
+        if (authData != null) {
+            token = authData.authToken();
+        }
+        return authData;
+    }
+
+    public void logout() throws Exception {
+        String path = "/session";
+        this.request("DELETE", path, null, null);
+        token = null;
+    }
+
+    public Integer create(String name) throws Exception {
+        String path = "/game";
+        return this.request("POST", path, name, Integer.class);
+    }
+
+    public void join(JoinRequest join) throws Exception {
+        String path = "/game";
+        this.request("POST", path, join, null);
+    }
+
+    public ArrayList<GameResult> list() throws Exception {
+        String path = "/game";
+        return this.request("GET", path, null, ListResult.class).games();
+    }
+
+    private <T> T request(String method, String path, Object request, Class<T> response) throws Exception {
+        try {
+            URL url = (new URI(this.url + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            if(token != null) {
+                http.setRequestProperty("Authorization", token);
+            }
+
+            write(request, http);
+            http.connect();
+            var status = http.getResponseCode();
+            if (status != 200) {
+                throw new Exception("failure: " + status);
+            }
+            return read(http, response);
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    private void write(Object request, HttpURLConnection http) throws Exception {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String requestString = new Gson().toJson(request);
+            try {
+                OutputStream os = http.getOutputStream();
+                os.write(requestString.getBytes());
+            } catch (Exception e) {
+                throw new Exception(e.getMessage());
+            }
+        }
+    }
+
+    private <T> T read(HttpURLConnection http, Class<T> response) throws Exception {
+        T res = null;
+        if (http.getContentLength() < 0) {
+            try {
+                InputStream stream = http.getInputStream();
+                InputStreamReader reader = new InputStreamReader(stream);
+                if(response != null) {
+                    res = new Gson().fromJson(reader, response);
+                }
+            } catch (Exception e) {
+                throw new Exception(e.getMessage());
+            }
+        }
+        return res;
+    }
 }
